7d07b16ada824c83e9cd4c6bc844b6512b5226a2
Mathilde Congiu <mathilde.congiu@hotmail.com>
2020-11-22 10:12:29 +0100

    Nothing

--- routes/privateAsso.js
+++ routes/privateAsso.js
@@ -35,6 +35,7 @@ router.put("/edit", isLoggedIn(), async (req, res, next) => {
 router.get("/search", isLoggedIn(), async (req, res, next) => {
   try {
     const businesses = await BusinessUser.find();
+    console.log(businesses)
     res.status(200).json(businesses);
   } catch (error) {
     res.json(error);
